955e58db143624d4157839d1ef3a565b7be93273
ImmortalIdiot <max.krasulya@yandex.ru>
2025-05-14 11:59:41 +0300

    refactor: small changes

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -12,7 +12,7 @@ fun main() {
     service.addProduct("Молоко", 200, LocalDate.now().plusDays(14), TemperatureMode.Normal, 100, 300, 50)
     service.addProduct("Яйца", 30, LocalDate.now().plusDays(3), TemperatureMode.Frozen, 20, 50, 10)
     service.addProduct("Хлеб", 11, LocalDate.now().plusDays(10), TemperatureMode.Chilled, 20, 60, 15)
-    service.addProduct("Рыба", 100, LocalDate.now().plusDays(20), TemperatureMode.Normal, 50, 150, 30)
+    service.addProduct("Пиво", 100, LocalDate.now().plusDays(20), TemperatureMode.Normal, 50, 150, 30)
     service.addProduct("Стейк из индейки", 80, LocalDate.now().minusDays(25), TemperatureMode.Normal, 40, 120, 25)
     service.addProduct("Рёбра Jack Daniels", 4, LocalDate.now().minusDays(60), TemperatureMode.Frozen, 10, 40, 5)
 
